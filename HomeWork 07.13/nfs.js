79058c823cffc6b564f1f4bcc67d38d919e186f3
Ratoi Vasile <36606424+bacbka93@users.noreply.github.com>
2018-07-16 18:48:30 +0300

    Add files via upload

--- HomeWork 07.13/nfs.js
+++ HomeWork 07.13/nfs.js
@@ -2,6 +2,7 @@ var road   = document.getElementById('road');
 var light1 = document.createElement('div');
 var light2 = document.createElement('div');
 var y      = 0;
+var x      = 0;
 var speed  = 5;
 var lights = false;
 
@@ -22,6 +23,11 @@ function createLights(){
 function move(){
   y+=speed;
   road.style.backgroundPosition='0px ' + y + 'px';
+  // land.style.backgroundPosition='0px ' + y + 'px';
+  car.style.left = x + 'px';
+  if ( x > 150 || x < -10 ){
+    speed *= 0.95;
+  }
 }
 
 
@@ -31,6 +37,12 @@ function action(){
   }
   if ( event.keyCode == 40 ){
     speed--;
+  }
+  if ( event.keyCode == 37 ){
+    x-= 10;
+  }
+  if ( event.keyCode == 39 ){
+    x += 10;
   }
   if ( event.keyCode == 76 ){
     lights = !lights;
