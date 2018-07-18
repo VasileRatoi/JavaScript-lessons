3d0e50598c9e215f9d26e92d0f42f86f41620b5e
Ratoi Vasile <36606424+bacbka93@users.noreply.github.com>
2018-07-18 19:33:48 +0300

    Add files via upload

---
+++ vorobey-dom/vorobey-dom/js/bird.js
@@ -0,0 +1,36 @@
+// COMPONENTS
+class Bird{
+  // creaza obiectul
+  constructor(){
+    this.top = Math.round( (500 - 50) * Math.random() );
+    this.left = Math.round( (500 - 50) * Math.random() );
+    this.color = 'red';
+
+    this.speed = Math.round( Math.random() * 4 + 1 );
+  }
+  // Afiseaza pasarea in html
+  render(){
+    this.div = document.createElement('div');
+    this.div.setAttribute('class', `bird ${this.color}` );
+    document.body.firstElementChild.appendChild( this.div );
+    // position. left, top
+
+    this.div.style.top = `${this.top}px`;
+    this.div.style.left = `${this.left}px`;
+  }
+  fly(){
+    var self = this;
+    setInterval(function(){
+      if ( self.left > (250 + Math.random() * 200) || self.left < Math.random() * 200 ){
+        self.speed *= -1;
+        if( Math.abs(self.speed) < 20 ){
+          self.speed *= 1.5;
+        }
+      }
+      //// functia in functie si atunci el nu vede this asa ca o salvam in prima functie in variabila
+      self.left+=self.speed;
+      self.div.style.left = `${self.left}px`;
+      console.log(self.left);
+    },20);
+  }
+}
