474cf7c28e5b3e516e0a5d47e09b63160009bc6b
Ratoi Vasile <36606424+bacbka93@users.noreply.github.com>
2018-08-03 19:29:52 +0300

    Add files via upload

---
+++ cvas-snake/js/game.js
@@ -0,0 +1,17 @@
+var canvas;
+
+////////////////////////Helpers/////////////////////////
+function randCoord(){
+    return Math.floor( Math.random() * 500 );
+}
+////////////////////////Helpers/////////////////////////
+
+function startGame(){
+    canvas = document.getElementById('map').getContext('2d');
+    var snake = new Snake(canvas);
+        snake.render(canvas);
+    var bomb = new Bomb(canvas);
+        bomb.render();
+        setTimeout(function(){ bomb.clear();}, 1000);
+}
+startGame();
